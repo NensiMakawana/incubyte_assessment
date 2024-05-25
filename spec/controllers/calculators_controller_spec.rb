6e2c2a5566c821b139cbda75f27fbd6ecb2f4869
NensiMakawana <nensi.makawana@gmail.com>
2024-05-25 11:58:02 +0530

    for add method failing test cases added

--- spec/controllers/calculators_controller_spec.rb
+++ spec/controllers/calculators_controller_spec.rb
@@ -2,5 +2,8 @@ require 'rails_helper'
 
 RSpec.describe CalculatorsController, type: :controller do
   context 'Addition' do
+    it "Allow empty string" do
+      post :add, params: { numbers: "" }
+    end
   end
 end
