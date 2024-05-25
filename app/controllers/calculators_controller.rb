e9b38dbfab04365488534f682ff63a8bc017c39d
NensiMakawana <nensi.makawana@gmail.com>
2024-05-25 13:38:20 +0530

    added code for string calculator

--- app/controllers/calculators_controller.rb
+++ app/controllers/calculators_controller.rb
@@ -1,4 +1,11 @@
 class CalculatorsController < ApplicationController
+  # method for string calculator addition
   def add
+    numbers = params[:numbers]
+    number_array = numbers.split(/[^0-9-]+/).map(&:to_i)
+    negatives_array = number_array.select{ |i| i<0 }
+    raise "cannot accept negatives" if negatives_array.length > 0
+    total = number_array.inject(0){|sum,x| sum + x }
+    render json: { sum: total }
   end
 end
